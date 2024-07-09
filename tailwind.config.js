4d0a1841c24d776d6840b2e3e377b1c7c2dbfedd
Fratila Andrei <andifratila121618@gmail.com>
2024-07-10 02:41:58 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "carrousel-color": "#F3F1EE",
+        "gray-color": "#595959",
+        "footer-color": "#D5D4D0",
+        "beige-color": "#B69454",
+        "white-color": "#FFFFFF",
+        "logo-color": "#272727",
+        "normal-black-color": "#262626",
+        "important-black-color": "#000000",
+      },
+      fontFamily: {
+        lora: ["Lora"],
+        roboto: ["Roboto"],
+      },
+    },
+  },
+  plugins: [],
+};
